3413a01a06458f0bd3ee614b5e7c042576583e30
Ali Murat Kuşlu <97320624+alimuratkuslu@users.noreply.github.com>
2026-02-22 12:48:48 +0300

    Update SHA256 for Dispatch cask
    Updated SHA256 checksum for Dispatch cask.

--- Casks/dispatch.rb
+++ Casks/dispatch.rb
@@ -1,6 +1,6 @@
 cask "dispatch" do
   version "1.0.0"
-  sha256 "7e8b8020d0f5efc0f0c0839cb3283a0370b0a37cf3b86958f65b684928cd5945"
+  sha256 "b11d646780648269158594bc79fec30bf758f98695e6507fcd4ed217611fb926"
 
   url "https://github.com/alimuratkuslu/dispatch/releases/download/v#{version}/Dispatch.zip"
   name "Dispatch"
