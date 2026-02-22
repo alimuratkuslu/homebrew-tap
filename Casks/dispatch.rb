ba0ef653d08adcb37fe93deb0de1c69c00d29d1e
Ali Murat Kuşlu <97320624+alimuratkuslu@users.noreply.github.com>
2026-02-22 12:49:45 +0300

    Update dispatch version to 1.2.1

--- Casks/dispatch.rb
+++ Casks/dispatch.rb
@@ -1,5 +1,5 @@
 cask "dispatch" do
-  version "1.0.0"
+  version "1.2.1"
   sha256 "b11d646780648269158594bc79fec30bf758f98695e6507fcd4ed217611fb926"
 
   url "https://github.com/alimuratkuslu/dispatch/releases/download/v#{version}/Dispatch.zip"
